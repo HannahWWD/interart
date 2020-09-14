f6eb71d7ce5ab0a693f22b70e54d2500491420ad
Hannah Ng <hannahng@hannahs-mbp-2.lan>
2020-09-13 17:23:57 -0700

    finished static website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import './components/Sidebar';
+import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar';
+import {BrowserRouter,Route} from 'react-router-dom'
+import ResetScroll from './components/ResetScroll';
+import Home from './pages/Home';
+import Article from './pages/Article';
+import All from './pages/All';
+import NewPost from './pages/NewPost';
+import MyArchive from './pages/MyArchive';
+import Collection from './pages/Collection';
+import MyPosts from './pages/MyPosts';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <BrowserRouter>
+        <Sidebar />
+        <Navbar />
+        <ResetScroll />
+       
+        <Route exact path="/" component={Home}/> 
+        <Route path="/article" component={Article}/> 
+        <Route path="/all" component={All}/>
+        <Route path="/new-post" component={NewPost}/>
+        <Route path="/archive" component={MyArchive}/>
+        <Route path="/collection" component={Collection}/>
+        <Route path="/my-posts" component={MyPosts}/>
+      
+
+      </BrowserRouter>
+
+    
     </div>
   );
 }
